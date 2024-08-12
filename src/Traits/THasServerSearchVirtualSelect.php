6e1a6ec21c495bfbce5c4d54f321290cb49b3c41
Blazik <jakub.blaha@involve.cz>
2024-08-12 07:57:06 +0200

    fix: added trait for virtual select options

---
+++ src/Traits/THasServerSearchVirtualSelect.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InvolveDigital\LaravelLivewireForms\Traits;
+
+use InvolveDigital\LaravelLivewireForms\Inputs\VirtualSelect;
+use ReflectionMethod;
+
+trait THasServerSearchVirtualSelect
+{
+
+    protected function sendNewVirtualSelectOptions(array $options): void
+    {
+        $calledFromData = debug_backtrace(~DEBUG_BACKTRACE_PROVIDE_OBJECT & ~DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1];
+
+        $serverSearchArguments = [];
+
+        $calledFromFunction = new ReflectionMethod($calledFromData['class'], $calledFromData['function']);
+        foreach ($calledFromFunction->getParameters() as $parameter) {
+            if ($parameter->getName() === 'searchTerm') {
+                unset($calledFromData['args'][$parameter->getPosition()]);
+            } else {
+                $serverSearchArguments[$parameter->getName()] = $calledFromData['args'][$parameter->getPosition()];
+            }
+        }
+
+        $onServerSearchHandler = $calledFromData['function'];
+
+        $eventId = VirtualSelect::generateEventIdForServerSearch($onServerSearchHandler, $serverSearchArguments);
+
+        $this->dispatch("serverOptions:$eventId", $options);
+    }
+
+}
